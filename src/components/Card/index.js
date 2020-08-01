cfa699fd606e937417619a760a85ec871fce4b71
Samuel Marques <samuelcabralmarques@gmail.com>
2020-08-01 03:14:06 -0300

    board, card, header, list

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7157c1" />
+      </header>
+      <p>Fazer migração completa</p>
+      <img src="https://api.adorable.io/avatars/285/abott@adorable.io.png" alt=""/>
+    </Container>
+  );
+}
+
---
+++ src/components/List/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { MdAdd }  from 'react-icons/md';
+
+import Card from '../Card';
+
+import { Container } from './styles';
+
+export default function List() {
+  return (
+    <Container>
+      <header>
+        <h2>Tarefas</h2>
+        <button type="button">
+          <MdAdd size={24} color="#FFF" />
+
+        </button>
+      </header>
+
+      <ul>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ul>
+    </Container>
+  );
+}
